3003c70e46b74b121b45de9c4255d634a1701fbc
dexboost <dexboost@gmail.com>
2025-01-11 02:09:00 +0200

    fix

--- src/config.prod.ts
+++ src/config.prod.ts
@@ -1,43 +1,32 @@
-import { Config } from './types';
-
-export const config: Config = {
+export const config = {
+    endpoints: [
+        {
+            name: "boosts-latest",
+            url: "https://api.dexscreener.com/token-boosts/latest/v1",
+            platform: "dexscreener"
+        },
+        {
+            name: "get-token",
+            url: "https://api.dexscreener.com/latest/dex/tokens/",
+            platform: "dexscreener"
+        }
+    ],
     settings: {
         api_get_timeout: 10000,
-        db_name_tracker: 'database.db',
+        db_name_tracker: '/home/dexboost/database.db',
         frontend_url: 'https://dexboost.xyz',
         api_url: 'https://api.dexboost.xyz',
-        dex_to_track: 'raydium',
+        dex_to_track: "raydium",
         min_boost_amount: 100,
-        chains_to_track: ['solana'],
+        chains_to_track: ["solana"],
         ignore_pump_fun: false,
         hunter_timeout: 5000
     },
-    bots: [
-      {
-        referral: "r-digitalbenjamins",
-        username: "TradeonNovaBot",
-        chain: "solana",
-      },
-    ],
     axios: {
-      get_timeout: 10000, // Timeout for API requests
-    },
-    dex: {
-      endpoints: [
-        {
-          platform: "dexscreener",
-          name: "boosts-latest",
-          url: "https://api.dexscreener.com/token-boosts/latest/v1",
-        },
-        {
-          platform: "dexscreener",
-          name: "get-token",
-          url: "https://api.dexscreener.com/latest/dex/tokens/",
-        },
-      ],
+        get_timeout: 10000
     },
     rug_check: {
-      enabled: true, // if set to false, the rugcheck will not be included in the response
-      verbose_log: false,
-    },
-  };
+        enabled: true,
+        verbose_log: false
+    }
+};
